1c645c95f1abd0c85d19bd697c5258f94972d3d4
Steven YON <steven.yon@outlook.fr>
2018-02-15 11:24:59 +0100

    Splash screen et home

--- app/src/main/java/com/example/bins/rookieagent/MainActivity.java
+++ app/src/main/java/com/example/bins/rookieagent/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.bins.rookieagent;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN; //Mini android 4.1
+        decorView.setSystemUiVisibility(uiOptions);
+
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
