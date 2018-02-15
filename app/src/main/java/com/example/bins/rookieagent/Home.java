820f130994faf736a19b2c9f2181e7c9e8d58b34
Steven YON <steven.yon@outlook.fr>
2018-02-15 15:29:07 +0100

    Home button works

--- app/src/main/java/com/example/bins/rookieagent/Home.java
+++ app/src/main/java/com/example/bins/rookieagent/Home.java
@@ -1,42 +1,69 @@
 package com.example.bins.rookieagent;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
-
-    private ImageView homeLogo;
-    private Button btnStart;
-    private Button btnQuit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        homeLogo = findViewById(R.id.homeLogo);
-        btnStart = findViewById(R.id.btnStart);
-        btnQuit = findViewById(R.id.btnQuit);
     }
+
 
     public void Commencer(View view) {
         Toast.makeText(Home.this, "Commencer", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, Home.class);
+        Intent intent = new Intent(this, Jeu.class);
         startActivity(intent);
     }
 
     public void Option(View view) {
-        Toast.makeText(Home.this, "Option", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.alert_dialog_option, null))
+                .setIcon(R.drawable.option).setTitle(R.string.option)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(Home.this, "Option enregistré", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 
     public void Quitte(View view) {
-        Toast.makeText(Home.this, "information: votre partie n'a pas été sauvegardé", Toast.LENGTH_SHORT).show();
-        finish();
-        System.exit(0);
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.exit_message)
+                .setIcon(R.drawable.ic_police).setTitle(R.string.exit)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish();
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 
+    public void sons(View view) {
+        if (view.isActivated()){
+            Toast.makeText(Home.this, "Sons déactivé", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(Home.this, "Sons activé", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
