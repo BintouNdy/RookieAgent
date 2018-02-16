34b608fd923f172f453f7ae3c25dd34f30218712
bintou n'diaye <bintou.ndy@gmail.com>
2018-02-16 15:17:05 +0100

    Personnage + ajout compétence

--- app/src/main/java/com/example/bins/rookieagent/Jeu.java
+++ app/src/main/java/com/example/bins/rookieagent/Jeu.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Jeu extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class Jeu extends AppCompatActivity {
     private Button btn_2_1;
     private Button btn_2_2;
 
+    Personnage perso = new Personnage(0, 1, 0, 0, 0, 0, 0, "Steven", "Yon");
     String quartier;
     int img;
     String localisation;
@@ -51,27 +55,181 @@ public class Jeu extends AppCompatActivity {
             case "Ville":
                 img = R.drawable.map_part1;
                 localisation = getString(R.string.circulation);
-                tab = new String[]{"chercher le café", "léché les bottes du chef", "aller chercher son arme", "Level UP"};
+                tab = new String[]{"Faire la circulation", "Surveiller le quartier", "Faire de la prévention", "Evènement"};
                 break;
             case "Police":
                 img = R.drawable.map_part1;
                 localisation = getString(R.string.police_station);
+                tab = new String[]{"Entrainement", "Lécher les bottes du capitaine", "Apporter le café", "Trouver une enquête"};
+                //region click
+                btn_1_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(RandomNumber());
+                        perso.addGunControl(RandomNumber());
+                        perso.addLogic(RandomNumber());
+                    }
+                });
+                btn_1_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomLecheur());
+                        perso.addMoral(RandomLecheur());
+                        perso.addExperience(RandomLecheur());
+                    }
+                });
+                btn_2_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(2);
+                    }
+                });
+                btn_2_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TO-DO
+                    }
+                });
+                //endregion
                 break;
             case "Gare":
                 img = R.drawable.map_part2;
                 localisation = getString(R.string.gare);
+                tab = new String[]{"Surveiller la foule", "Aider des touristes", "Enqueter", "Evènement"};
+                //region click
+                btn_1_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomNumber());
+                        perso.addLogic(RandomNumber());
+                    }
+                });
+                btn_1_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomNumber());
+                        perso.addMoral(RandomNumber());
+                        perso.addExperience(RandomNumber());
+                    }
+                });
+                btn_2_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(2);
+                    }
+                });
+                btn_2_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TO-DO
+                    }
+                });
+                //endregion
                 break;
             case "Hopital":
                 img = R.drawable.map_part2;
                 localisation = getString(R.string.hopital);
+                tab = new String[]{"Se soigner", "Parler au légiste", "Enqueter", "Evènement"};
+                //region click
+                btn_1_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(RandomNumber());
+                        perso.addGunControl(RandomNumber());
+                        perso.addLogic(RandomNumber());
+                    }
+                });
+                btn_1_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomNumber());//TO-DO Valeur négatif
+                        perso.addMoral(RandomNumber());//TO-DO Valeur négatif
+                        perso.addExperience(RandomNumber());//TO-DO Valeur négatif
+                    }
+                });
+                btn_2_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(2);
+                    }
+                });
+                btn_2_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TO-DO
+                    }
+                });
+                //endregion
                 break;
             case "Plage":
                 img = R.drawable.map_part3;
                 localisation = getString(R.string.plage);
+                tab = new String[]{"Surveiller les jeunes", "Faire un jogging", "Enqueter", "Evènement"};
+                //region click
+                btn_1_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(RandomNumber());
+                        perso.addGunControl(RandomNumber());
+                        perso.addLogic(RandomNumber());
+                    }
+                });
+                btn_1_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomNumber());//TO-DO Valeur négatif
+                        perso.addMoral(RandomNumber());//TO-DO Valeur négatif
+                        perso.addExperience(RandomNumber());//TO-DO Valeur négatif
+                    }
+                });
+                btn_2_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(2);
+                    }
+                });
+                btn_2_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TO-DO
+                    }
+                });
+                //endregion
                 break;
             case "Port":
                 img = R.drawable.map_part3;
                 localisation = getString(R.string.port);
+                tab = new String[]{"Aider la douane", "Surveiller les docks", "Enqueter", "Evènement"};
+                //region click
+                btn_1_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(RandomNumber());
+                        perso.addGunControl(RandomNumber());
+                        perso.addLogic(RandomNumber());
+                    }
+                });
+                btn_1_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addStressControl(RandomNumber());//TO-DO Valeur négatif
+                        perso.addMoral(RandomNumber());//TO-DO Valeur négatif
+                        perso.addExperience(RandomNumber());//TO-DO Valeur négatif
+                    }
+                });
+                btn_2_1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        perso.addExperience(2);
+                    }
+                });
+                btn_2_2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TO-DO
+                    }
+                });
+                //endregion
                 break;
             default:
                 img = R.drawable.img_404;
@@ -84,6 +242,18 @@ public class Jeu extends AppCompatActivity {
         btn_1_2.setText(tab[1]);
         btn_2_1.setText(tab[2]);
         btn_2_2.setText(tab[3]);
+    }
+
+    public int RandomNumber() {
+        Random randomNumber = new Random();
+        int random = randomNumber.nextInt(15);
+        return random;
+    }
+
+    public int RandomLecheur(){
+        Random randomNumber = new Random();
+        int random = randomNumber.nextInt(15 + 1 -15) + 15;
+        return random;
     }
 
     @Override
--- app/src/main/java/com/example/bins/rookieagent/Personnage.java
+++ app/src/main/java/com/example/bins/rookieagent/Personnage.java
@@ -1,8 +1,82 @@
 package com.example.bins.rookieagent;
 
-/**
- * Created by Bins on 16/02/2018.
- */
-
 public class Personnage {
+    int experience, level, gunControl, stressControl, moral, logic, health;
+    String name, surname;
+
+    public Personnage(int experience, int level, int gunControl, int stressControl, int health, int moral, int logic, String name, String surname) {
+        this.experience = experience;
+        this.level = level;
+        this.gunControl = gunControl;
+        this.stressControl = stressControl;
+        this.moral = moral;
+        this.logic = logic;
+        this.name = name;
+        this.surname = surname;
+        this.health = health;
+    }
+
+    // region Getter and Setter
+    public int getExperience() {
+        return experience;
+    }
+
+    public void addExperience(int experience) {
+        this.experience = this.experience + experience;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void addLevel(int level) {
+        this.level = this.level + level;
+    }
+
+    public int getGunControl() {
+        return gunControl;
+    }
+
+    public void addGunControl(int gunControl) {
+        this.gunControl = this.gunControl + gunControl;
+    }
+
+    public int getStressControl() {
+        return stressControl;
+    }
+
+    public void addStressControl(int stressControl) {this.stressControl = this.stressControl + stressControl;}
+
+    public int getMoral() {
+        return moral;
+    }
+
+    public void addMoral(int moral) {
+        this.moral = this.moral + moral;
+    }
+
+    public int getLogic() {
+        return logic;
+    }
+
+    public void addLogic(int logic) {
+        this.logic = this.logic + logic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    //endregion
 }
